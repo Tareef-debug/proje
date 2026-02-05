8feb57244370374c6bcee433f70f82b6518973ad
Tareef-debugger <tareeftoufique@gmail.com>
2026-02-05 14:53:33 +0600

    Add files via upload

---
+++ project.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+database = 'database.sqlite'
+
+conn = sqlite3.connect(database)
+print('Opened data successfully')
+
+
+
+import pandas as pd
+tables = pd.read_sql("""SELECT * FROM sqlite_master WHERE type='table';""",conn)
+
+
+tables
+
+
+matches = pd.read_sql("""SELECT * FROM Match;""",conn)
+
+
+matches.info()
